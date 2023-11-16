25bf3d2cae5d6f7fa2287fde8ef2137c5f0c671c
Arckness <66972966+Arckness@users.noreply.github.com>
2023-11-16 16:15:29 -0500

    Start of project

---
+++ src/CanvasPanel.java
@@ -0,0 +1,115 @@
+/**
+ * 2D CanvasPanel
+ * 
+ *
+ * @author (Prof R)
+ * @version (v1.0 11-17-22)
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CanvasPanel extends JPanel
+{
+    private final static int X_CORNER = 25;
+    private final static int Y_CORNER = 25;
+    private final static int CANVAS_WIDTH = 800;
+    private final static int CANVAS_HEIGHT = 400;
+    
+    // private List<Shape>
+    private Circle circle1;
+    private Circle circle2;
+    private int frameNumber;
+
+    
+    public CanvasPanel()
+    {
+        // Create some shapes, they should be in a List
+        circle1 = new Circle();  // Construct a circle with the default color
+        circle2 = new Circle(7); // Construct a circle with color index 7
+        
+        // Callback for keyboard events
+        this.setFocusable(true);
+        this.addKeyListener(new myActionListener());
+        System.out.println("keyboard event registered");
+        
+        // Create a render loop
+        // Create a Swing Timer that will tick 30 times a second
+        // At each tick the ActionListener that was registered via the lambda expression will be invoked
+        Timer renderLoop = new Timer(30, (ActionEvent ev) -> {frameNumber++; Simulate(); repaint();}); // lambda expression for ActionListener implements actionPerformed
+        renderLoop.start();
+    }
+    
+    public void Simulate()
+    {
+        circle1.Move(1, 2); // move the shape along via a delta in x and y
+        circle2.Move(2, 1); // move the shape along via a delta in x and y
+    }
+
+    // This method is called by renderloop
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        // Set window background to black
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,CANVAS_WIDTH + 2 * X_CORNER, CANVAS_HEIGHT + 2 * Y_CORNER); //draw the black border
+        
+        // Set canvas background to grey
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(X_CORNER, Y_CORNER, CANVAS_WIDTH, CANVAS_HEIGHT); //make the canvas white
+
+        // Need to make draw polymorphic and call draw for each shape in out list
+        circle1.draw(g);
+        circle2.draw(g);  
+    }
+    
+    public static int getCanvasWidth()
+    {
+        return CANVAS_WIDTH;
+    }
+
+    public static int getCanvasHeight()
+    {
+        return CANVAS_HEIGHT;
+    }
+
+    public static int getCanvasXBorder()
+    {
+        return X_CORNER;
+    }
+
+    public static int getCanvasYBorder()
+    {
+        return Y_CORNER;
+    }
+    public class myActionListener extends KeyAdapter 
+    {
+        public void keyPressed(KeyEvent e)
+        {
+            switch (e.getKeyCode())
+            {
+                case KeyEvent.VK_UP:
+                    System.out.println("press up arrow");
+                break;
+                case KeyEvent.VK_DOWN:
+                    System.out.println("press down arrow");
+                break;
+                case KeyEvent.VK_LEFT:
+                    System.out.println("press left arrow");
+                break;
+                case KeyEvent.VK_RIGHT:
+                    System.out.println("press right arrow");
+                break;
+                default:
+                    System.out.println("press some other key besides the arrow keys");
+            }
+        }
+        public void keyReleased(KeyEvent e)
+        {
+            System.out.println("released");
+        }
+    }
+}
---
+++ src/Project6_Game_Template_driver.java
@@ -0,0 +1,21 @@
+
+/**
+ * Project6 template driver class for frame(window), and panel(canvas)
+ * 
+ * template code for building your final project game
+ *
+ * @author (Nicholas Rua, Norah Micciulla)
+ * @version (v1.0, 11-16-2023)
+ */
+public class Project6_Game_Template_driver
+{
+   public static void main()
+   {
+       Game();
+   }
+   
+   public static void Game()
+   {
+       new CanvasFrame();  // construct the window, which will construct a frame (window), and a panel(canvas)
+   }
+}
