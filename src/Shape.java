68c2b9d6efc1fdbdfd3857bf6a3209a715f3246b
Cam0000929 <nmicciulla@gmail.com>
2023-11-22 10:23:02 -0500

    Added a Shape and Rectangle Class
    Added a Shape class to make code cleaner, added a Rectangle class for our projectiles and removed some code from the Circle class.

---
+++ src/Shape.java
@@ -0,0 +1,85 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public abstract class Shape {
+    public static final Color[] COLORS = {
+            //         R     G    B
+            new Color(255,   0,   0),  // Red     0
+            new Color(  0, 255,   0),  // Green   1
+            new Color(  0,   0, 255),  // Blue    2
+            new Color(  0,   0,   0),  // Black   3
+            new Color(128, 128, 128),  // Grey    4
+            new Color(255, 255, 255),  // White   5
+            new Color(255, 255,   0),  // Yellow  6
+            new Color(  0, 255, 255),  // Cyan    7
+            new Color(255,   0, 255),  // Magenta 8
+            new Color(165,  42,  42),  // Brown   9
+            new Color(255,  38,  38),
+            new Color(255, 168,  38),
+            new Color(212, 255,  38),
+            new Color( 82, 255,  38),
+            new Color( 38, 255, 125),
+            new Color( 38, 255, 255),
+            new Color( 38, 125, 255),
+            new Color( 82,  38, 255),
+            new Color(212,  38, 255),
+            new Color(255,  38, 168),
+    };
+
+    protected double xPosition;
+    protected double yPosition;
+    protected Color color;
+    protected int colorIndex;
+
+    /**
+     * More versatile Shape constructor, can now set the x and y position
+     *
+     * @param x
+     * @param y
+     * @param colorIndex
+     */
+    public Shape(int x, int y, int colorIndex) {
+        this.xPosition = x;
+        this.yPosition = y;
+        this.colorIndex = colorIndex;
+        this.color = COLORS[this.colorIndex];
+    }
+
+    /**
+     * Moves the shape its speed
+     */
+    public void Move(double xDelta, double yDelta) {
+        //move our speed
+        xPosition += xDelta;
+        yPosition += yDelta;
+    }
+
+    /**
+     * Gets the current x position of the shape
+     * @return the x position of the shape
+     */
+    public int GetX()
+    {
+        return (int)xPosition;
+    }
+
+    /**
+     * Gets the current y position of the shape
+     * @return the y position of the shape
+     */
+    public int GetY()
+    {
+        return (int)yPosition;
+    }
+
+    /**
+     * Gets the current color of the shape
+     * @return the color of the shape as a Color object
+     */
+    public Color GetColor()
+    {
+        return color;
+    }
+
+    public abstract void draw(Graphics g);
+}
